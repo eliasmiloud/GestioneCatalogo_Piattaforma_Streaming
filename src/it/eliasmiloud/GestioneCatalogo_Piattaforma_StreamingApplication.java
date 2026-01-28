bebed5d48ae760bc6af0a6671da14656b06cdd9b
e.miloudamar@itsprodigi.it <e.miloudamar@itsprodigi.it>
2026-01-28 17:26:28 +0100

    first commit

---
+++ src/it/eliasmiloud/GestioneCatalogo_Piattaforma_StreamingApplication.java
@@ -0,0 +1,10 @@
+package it.eliasmiloud;
+
+public class GestioneCatalogo_Piattaforma_StreamingApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
